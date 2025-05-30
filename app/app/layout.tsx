75f8e51953e96b26584ad2a2bef52c4d419d95bd
SedeqShelOr <SedeqShelOr@gmail.com>
2025-05-30 09:37:36 +0300

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,11 +1,1 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="he" dir="rtl">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
-    </html>
-  )
-}
+
